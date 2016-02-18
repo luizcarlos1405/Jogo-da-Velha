176ed4f905d61e2d9d72eecc8c604453105bda70
Luiz Carlos L. G. de Oliveira <luizcarlos1405@gmail.com>
2016-02-18 16:02:01 -0300

    Head comment correction

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 /******************************************************************************
-* Game Tic-Tac-Toe I.M. 2.0 by Luiz Carlos L. G. de Oliveira				  *
-*																			  *
+* Game Tic-Tac-Toe I.M. 2.0 by Luiz Carlos L. G. de Oliveira		      *
+*									      *
 * Feel free to modify at your will and share if want to. I've developed this  *
 * for practice.                                                               *
 * This update includes library ncurses.                                       *
-* 																			  *
-* Contact: luizcarlos1405@gmail.com 										  *
+* 									      *
+* Contact: luizcarlos1405@gmail.com 					      *
 *                                                                             *
- * Programa Jogo-da-Velha M.I. 2.0 criado por Luiz Carlos L. G. de Oliveira   *
- *                                                                            *
- * Sinta-se livre para modificar e compartilhar caso queira. Desenvolvi este  *
- * jogo para fins de prática apenas. Acabei me empolgando.                    *
- * Essa atualização inclui a utilização da biblioteca ncurses.                *
- *																			  *
- * Contato: luizcarlos1405@gmail.com                                          *
- *																			  *
- ******************************************************************************/
+* Programa Jogo-da-Velha M.I. 2.0 criado por Luiz Carlos L. G. de Oliveira    *
+*                                                                             *
+* Sinta-se livre para modificar e compartilhar caso queira. Desenvolvi este   *
+* jogo para fins de prática apenas. Acabei me empolgando.                     *
+* Essa atualização inclui a utilização da biblioteca ncurses.                 *
+*								              *
+* Contato: luizcarlos1405@gmail.com                                           *
+*									      *
+******************************************************************************/
 
 #include <stdio.h>
 #include <ncurses.h>
